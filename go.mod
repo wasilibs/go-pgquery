2a0738f55ce58f88796488fa17af83e2f96d260f
Anuraag Agrawal <anuraaga@gmail.com>
2024-01-23 10:22:21 +0900

    Replace wazerox with wazero

--- go.mod
+++ go.mod
@@ -5,8 +5,10 @@ go 1.19
 require (
 	github.com/google/go-cmp v0.5.5
 	github.com/pganalyze/pg_query_go/v5 v5.1.0
-	github.com/wasilibs/wazerox v0.0.0-20231208014050-e6b725634531
+	github.com/tetratelabs/wazero v1.3.0
 	google.golang.org/protobuf v1.31.0
 )
 
 require golang.org/x/xerrors v0.0.0-20191204190536-9bdfabe68543 // indirect
+
+replace github.com/tetratelabs/wazero => github.com/anuraaga/wazero v0.0.0-20240123011953-5c1baa8c3179
--- parser/parser_wazero.go
+++ parser/parser_wazero.go
@@ -11,12 +11,13 @@ import (
 	"runtime"
 	"sync"
 
+	"github.com/tetratelabs/wazero"
+	"github.com/tetratelabs/wazero/api"
+	"github.com/tetratelabs/wazero/experimental"
+	"github.com/tetratelabs/wazero/imports/wasi_snapshot_preview1"
+
 	"github.com/wasilibs/go-pgquery/internal/wasix_32v1"
 	"github.com/wasilibs/go-pgquery/internal/wasm"
-	wazero "github.com/wasilibs/wazerox"
-	"github.com/wasilibs/wazerox/api"
-	"github.com/wasilibs/wazerox/experimental"
-	"github.com/wasilibs/wazerox/imports/wasi_snapshot_preview1"
 )
 
 var (
